1f950ea63a607c06b588da329f8494fcc8e13132
Tanawat Khunlertkit <tanawat@ceas-942-imac17.cae.uwm.edu>
2018-11-13 17:59:43 -0600

    Update env

--- Lab_10/env/bin/django-admin.py
+++ Lab_10/env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/tkhunlertkit/Work/sketch/cs361f18/Lab_10/env/bin/python
+#!/Users/tanawat/Desktop/cs482/Lab_7/ProjectName/env/bin/python
 from django.core import management
 
 if __name__ == "__main__":
